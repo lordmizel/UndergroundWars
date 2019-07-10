9ab886eeabe95fcea4d47a6b729ca5ae6982e925
lordmizel <lordmizell@gmail.com>
2019-07-10 23:01:00 +0200

    Added limitations to cargo units based on other units movement type.

--- Assets/Scripts/Cargo.cs
+++ Assets/Scripts/Cargo.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Cargo : MonoBehaviour
 {
+    [SerializeField]
+    List<Unit.typeOfMovement> acceptedMovementTypes;
+
     // Start is called before the first frame update
     void Start()
     {
