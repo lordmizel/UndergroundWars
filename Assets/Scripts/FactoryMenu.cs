130a4b913a448dad487545602b6df33dd4c63fd7
lordmizel <lordmizell@gmail.com>
2020-01-01 20:00:05 +0100

    Added new game state for navigating the factory menu. Menu can now be deactivated as well.

--- Assets/Scripts/FactoryMenu.cs
+++ Assets/Scripts/FactoryMenu.cs
@@ -17,9 +17,23 @@ public class FactoryMenu : MonoBehaviour
 
     List<GameObject> activeButtons = new List<GameObject>();
 
+    int currentOption = 0;
+
     private void Awake()
     {
         instance = this;
+    }
+
+    private void FixedUpdate()
+    {
+        if (GameManager.gameState == GameManager.state.NAVIGATING_FACTORY_MENU)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                DeactivateFactoryMenu();
+                GameManager.gameState = GameManager.state.MOVING_CURSOR;
+            }
+        }
     }
 
     public void ActivateFactoryMenu(ClickableTile.factoryType factoryType) {
@@ -31,6 +45,13 @@ public class FactoryMenu : MonoBehaviour
         //TODO: FillFactory should take the type of factory as a parameter
         GameManager.instance.activePlayer.FillFactory(factoryType);
         menu.SetActive(true);
+        GameManager.gameState = GameManager.state.NAVIGATING_FACTORY_MENU;
+    }
+
+    void DeactivateFactoryMenu()
+    {
+        currentOption = 0;
+        menu.SetActive(false);
     }
 
     public void FillOption(int optionNumber, Unit unit)
