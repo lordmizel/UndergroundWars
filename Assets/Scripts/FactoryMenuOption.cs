a2052ccc54adf0c877a92067dd41f0a18963026a
lordmizel <lordmizell@gmail.com>
2020-04-08 08:42:05 +0200

    Streamlined the code for the factory option buttons.

---
+++ Assets/Scripts/FactoryMenuOption.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FactoryMenuOption : MonoBehaviour
+{
+    public Image unitSprite;
+    public Text unitName;
+    public Text unitPrice;
+}
