2d185e2f199ea7664f968b9b297fac3b40b2a604
lordmizel <lordmizell@gmail.com>
2021-02-06 21:42:56 +0100

    Created new scriptable object to store data of the battle maps.

---
+++ Assets/Scripts/BattleMap.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleMap : ScriptableObject
+{
+    public string mapName = "MapName";
+    public int playerNumber = 2;
+}
