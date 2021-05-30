f65aecd1d800f812705b43d059a10721ad61c248
lordmizel <lordmizell@gmail.com>
2021-05-30 22:40:20 +0200

    Added functions to change button color.

--- Assets/Scripts/MapMenuOption.cs
+++ Assets/Scripts/MapMenuOption.cs
@@ -6,9 +6,28 @@ using UnityEngine.UI;
 public class MapMenuOption : MonoBehaviour
 {
     public Text buttonText;
+    Image myImage;
+    Color initialColor, litColor;
+
+    private void Start()
+    {
+        myImage = this.GetComponent<Image>();
+        initialColor = myImage.color;
+        litColor = Color.yellow;
+    }
 
     public void ChangeName(string newName)
     {
         buttonText.text = newName;
+    }
+
+    public void LightUp()
+    {
+        myImage.color = litColor;
+    }
+
+    public void UnLight()
+    {
+        myImage.color = initialColor;
     }
 }
